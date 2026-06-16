d3145f85e134ac38e71b67b17e0a02fff51aeb16
nilsostertag <nostertag@gmx.de>
2026-06-17 03:40:17 +0800

    Added dependency installation scripts

---
+++ os_deps_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y \
+    python3-pip \
+    python3-venv \
+    python3-dev \
+    v4l-utils \
+    ffmpeg \
+    gstreamer1.0-tools \
+    libjpeg-dev
